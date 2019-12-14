925906de3b65a3278342025498353f9c5ecd42db
annastepanova <annastepanova1811@gmail.com>
2019-12-14 18:25:13 -0500

    fixed home route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import './App.css'
-import NavBar from "./Navbar"
-import './header.css'
-import Section from "./Section"
-import Header from "./Header"
-import Intro from "./Intro"
-import Skills from "./Skills"
-import MyForm from "./Myform"
+import { BrowserRouter, Route } from "react-router-dom"
+import Home from "./Home"
 
 
 class App extends React.Component {
@@ -14,34 +9,9 @@ class App extends React.Component {
 render(){
   return (
     <div className="App">
-      <NavBar />
-      <Header/>
-      <Section
-        title="Intro"
-        id="section1"
-      />
-      <Intro/>
-      <div className="container">
-      <Section
-        title="Skills"
-        id="section2"
-      />
-      <Skills/>
-      </div>
-      <Section
-        title="Portfolio"
-        id="section3"
-      />
-      <div className="container">
-      <Section
-        title="Contacts"
-        id="section4"
-      />
-      <MyForm/>
-      </div>
-      <Section
-        id="section5"
-      />
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+    </BrowserRouter>
     </div>
   )
 }
---
+++ src/Home.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import NavBar from "./Navbar"
+import './header.css'
+import Section from "./Section"
+import Header from "./Header"
+import Intro from "./Intro"
+import Skills from "./Skills"
+import MyForm from "./Myform"
+
+
+class Home extends React.Component {
+
+  render() {
+    return (
+        <>
+        <NavBar />
+        <Header />
+        <Section
+          title="Intro"
+          id="section1"
+        />
+        <Intro />
+        <div className="container">
+          <Section
+            title="Skills"
+            id="section2"
+          />
+          <Skills />
+        </div>
+        <Section
+          title="Portfolio"
+          id="section3"
+        />
+        <div className="container">
+          <Section
+            title="Contacts"
+            id="section4"
+          />
+          <MyForm />
+        </div>
+        <Section
+          id="section5"
+        />
+      </>
+    )
+  }
+}
+export default Home
